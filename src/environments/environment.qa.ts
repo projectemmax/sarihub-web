411e2e669aca7b61824775d90ebbf0e5a7b7d559
projectemmax <project.emmax@gmail.com>
2026-04-30 21:32:08 +0800

    update the enviroment

--- src/environments/environment.qa.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://qa-backend.onrender.com/api',
-  uploadsUrl: 'https://qa-backend.onrender.com/uploads/'
-};
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://staging-backend.onrender.com/api',
+  uploadsUrl: 'https://staging-backend.onrender.com/uploads/'
+};
