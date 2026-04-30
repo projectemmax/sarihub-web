47cea64d7793ae2aa76bd396d5181e40a18a334a
projectemmax <project.emmax@gmail.com>
2026-04-30 21:33:13 +0800

    update hero search

--- src/app/pages/storefront/components/hero/hero.component.ts
+++ src/app/pages/storefront/components/hero/hero.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hero',
+  imports: [FormsModule],
   standalone: true,
-  imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
 export class HeroComponent {
 
+    searchTerm = '';
+
+    constructor(private router: Router) {}
+
+    onSearch() {
+        const value = this.searchTerm?.trim();
+
+        if (!value) return;
+
+        this.router.navigate(['/storefront/shop'], {
+            queryParams: {
+                search: value,
+                page: 1
+            }
+        });
+    }
 }
