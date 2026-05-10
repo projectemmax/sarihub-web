7646b42ccff00651d78519cbee0bb67028e9166a
projectemmax <project.emmax@gmail.com>
2026-05-10 15:37:25 +0800

    implement the Shop Offers api in the template

--- src/app/pages/storefront/components/offers/offers.component.ts
+++ src/app/pages/storefront/components/offers/offers.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SiteConfigService } from '@app/core/services/site-config.service';
 
 @Component({
   selector: 'app-offers',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './offers.component.html',
   styleUrl: './offers.component.css'
 })
 export class OffersComponent {
+
+    readonly fallbackImage = '/assets/storefront/medical-ppe.jpg';
+
+    constructor(
+        private siteConfig: SiteConfigService
+    ) {}
+
+    get config() {
+        return this.siteConfig.snapshot ?? {};
+    }
+
+    get shopOffers(): any[] {
+        const offers = this.config?.shopOffers;
+
+        return Array.isArray(offers) ? offers : [];
+    }
+
+    trackByOffer(index: number, offer: any) {
+        return offer?.title ?? index;
+    }
 
 }
