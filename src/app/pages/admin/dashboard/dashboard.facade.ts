4d4fa22cc26679abab612e5d0d45b3e6e351b5fd
projectemmax <project.emmax@gmail.com>
2026-05-25 11:12:44 +0800

    feat(dashboard): add analytics loading skeleton

--- src/app/pages/admin/dashboard/dashboard.facade.ts
+++ src/app/pages/admin/dashboard/dashboard.facade.ts
@@ -1,12 +1,14 @@
 import { Injectable, signal } from '@angular/core';
 import { DashboardService } from '@app/services/admin/admin-dashboard.service';
 import { Analytics } from '@app/models/dashboard.model';
+import { delay } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DashboardFacade {
 
     range = signal<'1D' | '7D' | '1M' | '1Y'>('7D');
     analytics = signal<Analytics | null>(null);
+    analyticsLoading = signal(false);
 
     constructor(private dashboardService: DashboardService) {
         this.loadAnalytics(); // initial load
@@ -14,12 +16,20 @@ export class DashboardFacade {
 
     loadAnalytics() {
         const r = this.range();
+        this.analyticsLoading.set(true);
 
-        this.dashboardService.getAnalytics(r).subscribe(res => {
-            this.analytics.set(
-                res
-            );
-        });
+        this.dashboardService
+            .getAnalytics(r)
+            .subscribe({
+                next: (res) => {
+                    this.analytics.set(res);
+                    this.analyticsLoading.set(false);
+                },
+
+                error: () => {
+                    this.analyticsLoading.set(false);
+                }
+            });
     }
 
     setRange(r: '1D' | '7D' | '1M' | '1Y') {
